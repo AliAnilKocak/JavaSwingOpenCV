576dca66f1370c5c6402b487fb1e2d9ac33484ae
AliAnilKocak <alianilkocak@gmail.com>
2019-11-27 13:14:59 +0300

    Keskinleştirme class'ı yazıldı

--- src/opencv/FilterScreen.java
+++ src/opencv/FilterScreen.java
@@ -80,7 +80,7 @@ public class FilterScreen extends javax.swing.JFrame {
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
 
-        BufferedImage image = blurImage("C:\\Users\\alian\\Documents\\NetBeansProjects\\OpenCVProject\\src\\opencv\\images\\edge.png");    // TODO add your handling code here:
+        BufferedImage image = sharpenImage("C:\\Users\\alian\\Pictures\\Camera Roll\\WIN_20190821_11_31_33_Pro.jpg");    // TODO add your handling code here:
         imageBoxThirdScreen.setIcon(new ImageIcon(image));
 
     }//GEN-LAST:event_formWindowOpened
@@ -204,6 +204,85 @@ public class FilterScreen extends javax.swing.JFrame {
         image = op.filter(image, null);
 
         // Create frame with specific title
+        return image;
+    }
+    public BufferedImage sharpenImage(String imagePath){
+        ImageObserver myImageObserver = new ImageObserver() {
+
+            @Override
+            public boolean imageUpdate(Image image, int flags, int x, int y, int width, int height) {
+
+                if ((flags & ALLBITS) != 0) {
+
+                    imageLoaded = true;
+
+                    System.out.println("Image loading finished!");
+
+                    return false;
+
+                }
+
+                return true;
+
+            }
+        };
+
+        // The image URL - change to where your image file is located!
+        String imageURL = imagePath;
+
+        /**
+         *
+         * This call returns immediately and pixels are loaded in the background
+         *
+         * We use an ImageObserver to be notified when the loading of the image
+         *
+         * is complete
+         *
+         */
+        Image sourceImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+
+        sourceImage.getWidth(myImageObserver);
+
+        // We wait until the image is fully loaded
+        while (!imageLoaded) {
+
+            try {
+                Thread.sleep(100);
+
+            } catch (InterruptedException e) {
+            }
+
+        }
+
+        // Create a buffered image from the source image with a format that's compatible with the screen
+        GraphicsEnvironment graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+
+        GraphicsDevice graphicsDevice = graphicsEnvironment.getDefaultScreenDevice();
+
+        GraphicsConfiguration graphicsConfiguration = graphicsDevice.getDefaultConfiguration();
+
+        // If the source image has no alpha info use Transparency.OPAQUE instead
+        image = graphicsConfiguration.createCompatibleImage(sourceImage.getWidth(null), sourceImage.getHeight(null), Transparency.BITMASK);
+
+        // Copy image to buffered image
+        Graphics graphics = image.createGraphics();
+
+        // Paint the image onto the buffered image
+        graphics.drawImage(sourceImage, 0, 0, null);
+
+        graphics.dispose();
+
+        // A 3x3 kernel that sharpens an image
+        Kernel kernel = new Kernel(3, 3,
+                new float[]{
+                    -1, -1, -1,
+                    -1, 9, -1,
+                    -1, -1, -1});
+
+        BufferedImageOp op = new ConvolveOp(kernel);
+
+        image = op.filter(image, null);
+
         return image;
     }
 }
